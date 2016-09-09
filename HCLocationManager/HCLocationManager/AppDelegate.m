1c277ddcd247965573ea443363b7005a6ef83dc8
卧龙 <alicks@MacdeiMac.local>
2016-09-09 14:29:16 +0800

    添加开始

--- HCLocationManager/HCLocationManager/AppDelegate.m
+++ HCLocationManager/HCLocationManager/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"开始");
     return YES;
 }
 
